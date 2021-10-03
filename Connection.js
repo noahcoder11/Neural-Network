cb98f1f1b1fcff3de5c297e62e16fd70541990fe
Noah Hester <48141165+noahcoder11@users.noreply.github.com>
2021-10-03 13:28:56 -0700

    Create Connection.js
    Create connection class

---
+++ Connection.js
@@ -0,0 +1,14 @@
+class Connection {
+  constructor(weight){
+    this.weight = weight || Math.random()
+  }
+  forward(input){
+    var output = [];
+    
+    for(var i = 0;i < input.length;i++){
+      output[i] = input[i] * this.weight
+    }
+    
+    return this.output
+  }
+}
